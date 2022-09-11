8bb085c6a5d462cb60b3fd3cca1d9e932191df6a
Swati Raghav <38935203+swaticraghav@users.noreply.github.com>
2022-09-11 15:40:22 -0400

    Update the file

--- src/transpose.cpp
+++ src/transpose.cpp
@@ -1,36 +1,90 @@
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
 
-#include<iostream>
 using namespace std;
-#define N 4
 
-// This function stores transpose of A[][] in B[][]
-void transpose(int A[][N], int B[][N])
+void parsec_roi_begin() 
 {
-    int i, j;
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-            B[i][j] = A[j][i];
+
 }
- 
-// Driver code
-int main()
+
+void parsec_roi_end() 
 {
-    int A[N][N] = { {1, 1, 1, 1},
-                    {2, 2, 2, 2},
-                    {3, 3, 3, 3},
-                    {4, 4, 4, 4}};
- 
-    int B[N][N], i, j;
- 
-    transpose(A, B);
- 
-    cout <<"Result matrix is \n";
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < N; j++)
-           cout <<" "<< B[i][j];
-        cout <<"\n";
-    }
- 
-    return 0;
+
+}
+
+vector <vector <int> > read_matrix(string filename){
+	vector <vector <int> > A;
+	string line;
+	ifstream infile;
+	infile.open(filename.c_str());
+	int i = 0;
+		while (getline(infile, line) && !line.empty()) {
+			istringstream iss(line);
+			A.resize(A.size() + 1);
+			int a, j = 0;
+			while (iss >> a) {
+				A[i].push_back(a);
+				j++;
+			}
+			i++;
+		}
+	infile.close();
+	return A;
+
+}
+
+
+vector <vector<int> > matrix_transpose(vector< vector<int> > A){
+	int rows = A.size();
+	int columns = A[0].size();
+
+	vector <int> temp(rows, 0);
+	vector <vector <int> > result(columns, temp);
+
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < columns; j++){
+			result[i][j] = A[j][i];
+		}
+	}
+
+	return result;
+}
+
+void printMatrix(vector< vector<int> > matrix) {
+	vector< vector<int> >::iterator it;
+	vector<int>::iterator inner;
+	for (it=matrix.begin(); it != matrix.end(); it++) {
+		for (inner = it->begin(); inner != it->end(); inner++) {
+			cout << *inner;
+			if(inner+1 != it->end()) {
+				cout << "\t";
+			}
+		}
+		cout << endl;
+	}
+}
+
+int main (int argc, char* argv[]) {
+	string filename;
+
+	cout << argv[0];
+	if (argc < 3) {
+		filename = "mat_transpose.in";
+	} else {
+		filename = argv[2];
+	}
+
+	// cout << filename;
+	vector <vector <int> > A = read_matrix(filename);
+	printMatrix(A);
+    parsec_roi_begin();
+	vector< vector<int> > A_transpose = matrix_transpose(A);
+    parsec_roi_end();
+	printMatrix(A_transpose);
+	return 0;
 }
